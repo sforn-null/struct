89add20184048764728b5f8a8a1136609e23b382
sforn-null <1641832926@qq.com>
2021-03-13 23:09:47 +0800

    枚举

---
+++ 枚举.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+//enum 枚举关键字
+
+//enum Day  //枚举类型里面放的是所有可能变量的取值
+//{
+//	Mon,
+//	Tues,
+//	Wes,
+//	Thur,
+//	Fri,
+//	Sat,
+//	Sun   //最后一个不加逗号
+//};
+
+//enum Color
+//{
+//	red,
+//	green,
+//	blue
+//};
+//
+//int main()
+//{
+//	enum Color c = red;
+//	return;
+//}
